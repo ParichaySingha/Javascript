c163b1a3c3b11609cdebc289e8a5a75497d0bc03
Parichay <parichaysingha84532@gmail.com>
2025-06-05 12:26:46 +0530

    new data type opration learning

--- 01_basics/04_oprations.js
+++ 01_basics/04_oprations.js
@@ -21,6 +21,31 @@ console.log(y2);
 
 let gameCount = 100
 const newGameCount = ++gameCount;
-
 console.log(gameCount); // 101
   
+
+console.log("......................................");
+
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null == 0);
+
+
+console.log("......................................");
+
+let value = 10
+const value2 = 20
+
+console.log(value == value2); // false
+
+
+console.log("..........== or ===............");
+
+let m = new Number(10);
+let n = new Number(10);
+
+console.log(m == n);
+
+
